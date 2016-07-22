714cef58c8b1e83bbc5b9d2efac7a4db0d0f195b
K <Kai@Kais-MacBook-Air.local>
2016-07-22 11:05:29 -0700

    initial commit
    yep

--- testingArrayforFI2/GameScene.swift
+++ testingArrayforFI2/GameScene.swift
@@ -15,7 +15,10 @@ class GameScene: SKScene {
         myLabel.text = "Hello, World!"
         myLabel.fontSize = 45
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
+
+/////////////////
+		////////////////////////////
+		
         self.addChild(myLabel)
     }
     
@@ -36,9 +39,36 @@ class GameScene: SKScene {
             sprite.runAction(SKAction.repeatActionForever(action))
             
             self.addChild(sprite)
+			
+			var infoForLevels = [[[0, 60, 0, 0, 1], [0, 4, 0, 0, 1]], [[0, 7, 0, 0, 1], [0, 6, 0, 0, 1], [0, 5, 0, 0, 1]]]
+			//infoForLevels Example has three two levels, the first having two rings, and the second having three. Each ring has their defined properties
+			var arrayOfCircleImages = ["blackRingSetOne", "blackRingSetTwo", "blackRingSetThree", "blackRingSetFour", "blackRingSetFive"]
+			
+			print("infoForLevels = [[[0, 60, 0, 0, 1], [0, 4, 0, 0, 1]], [[0, 7, 0, 0, 1], [0, 6, 0, 0, 1], [0, 5, 0, 0, 1]]]")
+			print("arrayOfCircleImages = ['blackRingSetOne', 'blackRingSetTwo', 'blackRingSetThree', 'blackRingSetFour', 'blackRingSetFive']")
+			print("writing infoForLevels[0].count gets me \(infoForLevels[0].count)")
+			print("writing infoForLevels[0] gets me \(infoForLevels[0])")
+			print("writing infoForLevels[0] gets me \(infoForLevels[0])")
+			print("writing arrayOfCircleImages[0] gets me \(arrayOfCircleImages[0])")
+			print("writing infoForLevels[0][0][1] gets me \(infoForLevels[0][0][1])")
+			
+			
+			
+		
+			
+			
+			
+			
+	/////////////////////////////
+		
+/////////////////////////////////
+			
         }
     }
-   
+	
+/////////////////////////////
+	
+/////////////////////////////////
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
